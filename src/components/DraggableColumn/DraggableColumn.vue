f1f5badbc75e09e2ec1f1e18e4e6273f0d26daba
Evgeny <kholoshax@mail.ru>
2023-02-19 17:26:42 +0300

    Added main components

--- src/components/DraggableColumn/DraggableColumn.vue
+++ src/components/DraggableColumn/DraggableColumn.vue
@@ -1,17 +1,24 @@
 <template>
   <div class="column">
     <div class="column__info">
-      <div class="column__amount">14</div>
-      <div class="column__name">ToDo</div>
+      <div>ToDo</div>
+      <div class="column__info-amount">14</div>
+    </div>
+    <div class="column__tasks">
+      <Task />
     </div>
   </div>
 </template>
 
 <script>
 import { defineComponent } from "vue";
+import Task from "@/components/Task/Task";
 
 export default defineComponent({
   name: "DraggbleColumn",
+  components: {
+    Task,
+  },
 });
 </script>
 
---
+++ src/components/Task/Task.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="task">
+    <div class="task__name">Implement collector name</div>
+    <div class="task__info">
+      <div class="d-flex">
+        <div class="task__info-img"></div>
+        <div class="task__info-name">NUC-205</div>
+      </div>
+      <div class="task__info-priority"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "TaskKanboard",
+});
+</script>
+
+<style scoped lang="scss" src="./Task.scss">
+</style>
